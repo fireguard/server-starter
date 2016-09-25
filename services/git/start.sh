d91b530e3fbbc84bdf805f49c3782a167c387ed1
Ronaldo Meneguite <ronaldo@fireguard.com.br>
2016-09-25 19:21:30 -0300

    Add set git user config

--- services/git/start.sh
+++ services/git/start.sh
@@ -3,6 +3,9 @@ source /vagrant/scripts/functions.sh
 
 FILELOG=$1
 
+USER_NAME_GIT=$3
+EMAIL_GIT=$4
+
 printHeader "Start Git Service"
 
 if ! grep -q "git-core/ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
@@ -24,3 +27,7 @@ if [ "$INSTALED" == "false" ]; then
 else
   printSuccess "Git is installed"
 fi
+
+printSuccess "Configuring Git Client : ${USER_NAME_GIT} <${EMAIL_GIT}>"
+git config --global user.email "${EMAIL_GIT}" >> "${FILELOG}" 2>&1
+git config --global user.name "${USER_NAME_GIT}" >> "${FILELOG}" 2>&1
