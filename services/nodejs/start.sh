80cf28c466751b79ca52fcb8672e1e6b98ed0e1c
Ronaldo Meneguite <ronaldo@fireguard.com.br>
2016-09-25 19:35:02 -0300

    Create nodejs service

---
+++ services/nodejs/start.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+FILELOG=$1
+VERSION=$3
+
+source /vagrant/scripts/functions.sh
+
+printHeader "Start NodeJs Service"
+
+INSTALED=$( checkIsInstaled nodejs )
+if [ "$INSTALED" == "false" ]; then
+  if [ "$VERSION" == "6" ] || [ "$VERSION" == "default" ]; then
+    printSuccess "Install NodeJs 6"
+    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - >> "${FILELOG}" 2>&1
+    apt-get install -y nodejs >> "${FILELOG}" 2>&1
+    apt-get install -y build-essential >> "${FILELOG}" 2>&1
+
+  elif [ "$VERSION" == "4" ]; then
+    printSuccess "Install NodeJs 4"
+    curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash - >> "${FILELOG}" 2>&1
+    apt-get install -y nodejs >> "${FILELOG}" 2>&1
+    apt-get install -y build-essential >> "${FILELOG}" 2>&1
+  else
+    printError "${VERSION} is not valid"
+  fi
+else
+  printSuccess "NodeJs is installed"
+fi
